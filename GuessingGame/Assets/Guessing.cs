392df536c61cc8415953a5baab485dc162d3fc4a
Unknown <ryleyburnett@live.com>
2017-08-31 11:16:18 -0600

    Script, Level, ETC
    changes 01

---
+++ GuessingGame/Assets/Guessing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guessing : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Guesser");
+		print ("Pick a Number in Your Head");
+
+		print ("The Highest Number You Can Pick is 100");
+		print ("The Lowest Number You Can Pick is 100");
+
+		print ("Is the Number Higher or Lower Than 50?");
+		print ("Up Arrow for Higher, Down Arrow for Lower, & Enter for Equal");
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
