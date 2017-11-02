b3d96ac4a70ac9dd840e3480ff184a93ae8c0f7e
Unknown <ryleyburnett@live.com>
2017-11-02 11:20:45 -0600

    SpaceGameStart
    Just created a game

---
+++ SpaceGame/Assets/Scripts/MeteorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour {
+
+	public float startingSpin;
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().AddTorque (Random.Range(-startingSpin,startingSpin), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
---
+++ SpaceGame/Assets/Scripts/health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour 
+
+{
+
+	public int myHealth;
+	public Sprite[] picture;
+	private int count = 0;
+	private LevelManager levelManager;
+
+	void Start(){
+		levelManager = FindObjectOfType<LevelManager> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D myCollider) {
+
+		//take away health
+
+		myHealth--;
+
+		//if health is < 0 destroy brick
+
+		if (myHealth <= 0) {
+			LevelManager.brickCount--;
+			levelManager.CheckBrickCount ();
+			Destroy (this.gameObject);
+		}
+
+	}
+
+
+}
