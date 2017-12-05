2b242e5d099f895a3245857dbfebfc9f1c82235b
Unknown <ryleyburnett@live.com>
2017-12-05 10:20:29 -0700

    Block Breaker Build

--- SpaceGame/Assets/Scripts/EnemyAi.cs
+++ SpaceGame/Assets/Scripts/EnemyAi.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour {
 public GameObject[] obj;
-     public float spawnMin = 1f; //spawn aleatoriamente entre 1s
-     public float spawnMax = 2f; //e 2s
+public float spawnMin = 1f; //spawn aleatoriamente entre 1s
+public float spawnMax = 2f; //e 2s
 	// Use this for initialization
 	void Start () {
 		
@@ -15,9 +15,4 @@ public GameObject[] obj;
 	void Update () {
 		
 	}
-	void Spawn()
-     {
-         Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
-         Invoke("Spawn", Random.Range(spawnMin, spawnMax));
-     }
 }
