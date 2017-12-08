b378ca5798e172d2e1d3ab8145ed464359ea666b
Unknown <ryleyburnett@live.com>
2017-12-08 13:44:09 -0700

    Timer Add

---
+++ SpaceGame/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+	public GameObject spawn;
+	public float timer;
+	private float timerSave;
+	
+	// Use this for initialization
+	void Start () {
+		timerSave = timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0){
+			timer =timerSave;
+		}
+		if (timer == 10){
+			print ("Yo");
+		}
+	}
+	
+	
+}
