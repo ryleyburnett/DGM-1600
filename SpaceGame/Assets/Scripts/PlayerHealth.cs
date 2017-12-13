040edae17fe4a780f54610b67d85c37f8cfe99b1
Unknown <ryleyburnett@live.com>
2017-12-12 17:47:28 -0700

    Added Player Health
    separate health to go to game over

---
+++ SpaceGame/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int myHealth;
+	public GameObject explosionEffect;
+	public GameObject explosionEffect2;
+	public GameObject[] hearts;
+	public Sprite[] picture;
+	private int count = 0;
+	private LevelManager levelManager;
+	public Text scoreboard;
+
+	void Start(){
+		levelManager = FindObjectOfType<LevelManager> ();
+	
+	
+	}
+	public void LoadLevel (string name){
+		SceneManager.LoadScene (name);
+
+
+	}
+	public void LoadNextLevel (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
+
+	}
+	
+	
+
+	void OnCollisionEnter2D (Collision2D myCollider) {
+		Instantiate(explosionEffect2, myCollider.transform.position, Quaternion.identity);
+			//take away health
+
+			myHealth--;
+
+
+		if (myHealth <= 0) {
+			Destroy (this.gameObject);
+			Instantiate(explosionEffect, gameObject.transform.position, Quaternion.identity);
+			LoadNextLevel ();
+			
+			//if (!Player ()){
+			//IncrementScore();
+			//}
+		}
+
+	}
+	
+	void OnTriggerEnter2D (Collider2D myCollider) {
+		Instantiate(explosionEffect2, myCollider.transform.position, Quaternion.identity);
+			//take away health
+
+			myHealth--;
+
+		
+		if (myHealth <= 0) {
+			Destroy (this.gameObject);
+			Instantiate(explosionEffect, gameObject.transform.position, Quaternion.identity);
+			LoadNextLevel ();
+		}
+
+	}
+	
+	
+	
+	
+	private void IncrementScore(){
+	
+	scoreboard.text += 10;
+}
+
+}
